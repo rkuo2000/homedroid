e05842745c679a1d7ec91327b59d333ddc8eeb0b
Richard Kuo <rkuo2000@gmail.com>
2018-01-04 15:35:03 +0800

    Add files via upload

--- homedroid.py
+++ homedroid.py
@@ -1,28 +1,30 @@
 #-*- coding: utf-8 -*-
 #
-# HomeDroid - Dialogflow
+# Homedroid
 #
+# Features:
+#     using messenger to input commands
+#     homedroid.py running on PC to send commands from UART to NodeMCU
 import os
 import requests
 from sys import argv
 from bottle import Bottle, request
 from bs4 import BeautifulSoup
-import apiai
 import json
+import re
 import serial
-ser = serial.Serial('COM4', 9600)
+ser = serial.Serial('COM10', 9600)
 
-FB_PAGE_TOKEN = "EAANFvjT1XoY"
+FB_PAGE_TOKEN = "EAANFvjT1XoYBAHNOW"
 FB_VERIFY_TOKEN = "verify me"
 FB_GRAPH_API = 'https://graph.facebook.com/v2.10/me/messages?'
-ApiAI_ACCESS_TOKEN = "9ef660"
 
 # Setup Bottle Server
 app = Bottle()
 
 @app.get('/')
 def hello():
-    return "HomeDroid: Hello !"
+    return "Homebot: Hello !"
 
 @app.get('/webhook')
 def messenger_webhook():
@@ -38,19 +40,38 @@ def messenger_post():
     Handler for webhook (currently for postback and messages)
     """
     data = request.json
-    #print(data)
-    if data['originalRequest']['data']:
-        sender = data['originalRequest']['data']['sender']['id']
-        message = data['originalRequest']['data']['message'] 
-        text    = data['originalRequest']['data']['message']['text']
-        print(sender, text)
-        text = "robot moving"
-        sendMessage(sender, text) # to Messenger
-        print(text)
-        ser.write(b'forward')     # to UART
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            messages = entry['messaging']
+            if messages[0]:
+                sender  = messages[0]['sender']['id']
+                try:
+                    message = messages[0]['message']
+#-------------------Message Text-----------------------------------------------
+                    try:
+                        text  = message['text']
+                        print(sender,text)
+                        if text=='Stop':
+                            ser.write(b'stop\n')
+                        if text=='Forward':
+                            ser.write(b'forward\n')
+                        if text=='Backward':
+                            ser.write(b'backward\n')
+                        if text=='TurnRight':
+                            ser.write(b'turnRight\n')
+                        if text=='TurnLeft':
+                            ser.write(b'turnLeft\n')							
+                    except KeyError:
+                        print('>>>KeyError=message-text!')							
+                    except KeyError:
+                        print('>>>KeyError=message-quick_reply!')
+                except KeyError:
+                    print('>>>KeyError=message!')
+                except KeyError:
+                    print('>>>KeyError=postback!')
     else:
         return 'Received Different Event'
-	
+
 def sendMessage(sender, text):
 	"""
 	Function for sending message
@@ -61,7 +82,7 @@ def sendMessage(sender, text):
 	}
 	qs = 'access_token=' + FB_PAGE_TOKEN
 	resp = requests.post(FB_GRAPH_API + qs, json=data)
-	return resp.content
-
+	return resp.content	
+	
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=argv[1])
