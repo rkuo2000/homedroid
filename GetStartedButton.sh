d0c0b0e873007057dd167909574efaf14ec4b4b9
Richard Kuo <rkuo2000@gmail.com>
2018-07-01 17:48:58 +0800

    Delete GetStartedButton.sh

--- GetStartedButton.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST -H "Content-Type: application/json" -d '{
-  "get_started":
-    {
-      "payload":"GET_STARTED_PAYLOAD"
-	}
-}' "https://graph.facebook.com/v2.10/me/messenger_profile?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxx"
