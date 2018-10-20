884770f8e94a89347612a60a5b1b2ee4a6d225ce
Richard Kuo <rkuo2000@gmail.com>
2018-10-21 03:28:30 +0800

    Add files via upload

--- fbchat_basic.py
+++ fbchat_basic.py
@@ -1,12 +1,8 @@
-# -*- coding: UTF-8 -*-
-
 from fbchat import Client
 from fbchat.models import *
 
-client = Client('user_email', 'user_password')
+client = Client('your_email', 'your_password')
 
 print('Own id: {}'.format(client.uid))
-
 client.send(Message(text='Hi me!'), thread_id=client.uid, thread_type=ThreadType.USER)
-
 client.logout()
