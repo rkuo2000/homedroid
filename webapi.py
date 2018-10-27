e07826f79ff62fcca71ab04f0fbdd0204ff14341
Richard Kuo <rkuo2000@gmail.com>
2018-10-27 15:21:03 +0800

    Add files via upload

---
+++ webapi.py
@@ -0,0 +1,24 @@
+from sys import argv
+from bottle import Bottle, request
+import socket
+
+# Web API
+app = Bottle()
+@app.get('/hello')
+def hello():
+    return "Hello World!"
+
+@app.get('/stop')
+def stop():
+    return "Stop!"
+
+@app.get('/forward')
+def forward():
+    return "Moving Forward!"
+
+@app.get('/backward')
+def backward():
+    return "Moving Backward!" 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
