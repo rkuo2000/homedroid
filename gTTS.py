c27d121828e2114f9005c4ee25490523529af81f
Richard Kuo <rkuo2000@gmail.com>
2018-11-17 01:58:08 +0800

    Update gTTS.py

--- gTTS.py
+++ gTTS.py
@@ -4,5 +4,7 @@ import os
 
 tts = gTTS('bonjour',lang='fr')
 tts.save('gTTS.mp3')
-#
-os.system('vlc gTTS.mp3')
+
+#os.system('madplay gTTS.mp3') # RPi3
+os.system('cmdmp3 gTTS.mp3')     # PC
+#os.system('afplay gTTS.mp3')   # MAC
