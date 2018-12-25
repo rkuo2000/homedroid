8e1cfd2c8f28b3a59b596f34841213aaa175dcca
Richard Kuo <rkuo2000@gmail.com>
2018-12-25 21:05:08 +0800

    Update gTTS.py

--- gTTS.py
+++ gTTS.py
@@ -1,10 +1,16 @@
 ### $pip3 install gTTS
+## $python3 gTTS.py en hello
 from gtts import gTTS
 import os
+import sys
 
-tts = gTTS('bonjour',lang='fr')
+sl = sys.argv[1]
+text = sys.argv[2]
+
+tts = gTTS(text, lang=sl)
 tts.save('gTTS.mp3')
 
+#os.system('mpg321 gTTS.mp3') # PiZero
 #os.system('madplay gTTS.mp3') # RPi3
 os.system('cmdmp3 gTTS.mp3')     # PC
 #os.system('afplay gTTS.mp3')   # MAC
