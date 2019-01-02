e3c13a92912d0eecbc601c7befb4714a7f8628d2
Richard Kuo <rkuo2000@gmail.com>
2019-01-03 04:24:34 +0800

    Add files via upload

--- rpi3_gpio.py
+++ rpi3_gpio.py
@@ -3,48 +3,52 @@ import RPi.GPIO as GPIO
 import time
 
 # define motor control pins
-motorL_pin0 = 17
-motorL_pin1 = 27
-motorR_pin0 = 23
-motorR_pin1 = 24
+MotorL_pin0 = 17
+MotorL_pin1 = 27
+MotorR_pin0 = 23
+MotorR_pin1 = 24
+Sensor_Front_pin = 4
+Sensor_Sides_pin = 22
 
 # GPIO setup for motor control pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(motorL_pin0, GPIO.OUT)
-GPIO.setup(motorL_pin1, GPIO.OUT)
-GPIO.setup(motorR_pin0, GPIO.OUT)
-GPIO.setup(motorR_pin1, GPIO.OUT)
+GPIO.setup(MotorL_pin0, GPIO.OUT)
+GPIO.setup(MotorL_pin1, GPIO.OUT)
+GPIO.setup(MotorR_pin0, GPIO.OUT)
+GPIO.setup(MotorR_pin1, GPIO.OUT)
+GPIO.setup(Sensor_Front_pin, GPIO.IN)
+GPIO.setup(Sensor_Sides_pin, GPIO.IN)
 
 # Robo functions
 def robo_stop():
-    GPIO.output(motorL_pin0, GPIO.LOW)
-    GPIO.output(motorL_pin1, GPIO.LOW)
-    GPIO.output(motorR_pin0, GPIO.LOW)
-    GPIO.output(motorR_pin1, GPIO.LOW)
+    GPIO.output(MotorL_pin0, GPIO.LOW)
+    GPIO.output(MotorL_pin1, GPIO.LOW)
+    GPIO.output(MotorR_pin0, GPIO.LOW)
+    GPIO.output(MotorR_pin1, GPIO.LOW)
 
 def robo_forward():
-    GPIO.output(motorL_pin0, GPIO.LOW)
-    GPIO.output(motorL_pin1, GPIO.HIGH)
-    GPIO.output(motorR_pin0, GPIO.LOW)
-    GPIO.output(motorR_pin1, GPIO.HIGH)
+    GPIO.output(MotorL_pin0, GPIO.LOW)
+    GPIO.output(MotorL_pin1, GPIO.HIGH)
+    GPIO.output(MotorR_pin0, GPIO.LOW)
+    GPIO.output(MotorR_pin1, GPIO.HIGH)
 
 def robo_backward():
-    GPIO.output(motorL_pin0, GPIO.HIGH)
-    GPIO.output(motorL_pin1, GPIO.LOW)
-    GPIO.output(motorR_pin0, GPIO.HIGH)
-    GPIO.output(motorR_pin1, GPIO.LOW)
+    GPIO.output(MotorL_pin0, GPIO.HIGH)
+    GPIO.output(MotorL_pin1, GPIO.LOW)
+    GPIO.output(MotorR_pin0, GPIO.HIGH)
+    GPIO.output(MotorR_pin1, GPIO.LOW)
 
 def robo_left():
-    GPIO.output(motorL_pin0, GPIO.HIGH)
-    GPIO.output(motorL_pin1, GPIO.LOW)
-    GPIO.output(motorR_pin0, GPIO.LOW)
-    GPIO.output(motorR_pin1, GPIO.HIGH)
+    GPIO.output(MotorL_pin0, GPIO.HIGH)
+    GPIO.output(MotorL_pin1, GPIO.LOW)
+    GPIO.output(MotorR_pin0, GPIO.LOW)
+    GPIO.output(MotorR_pin1, GPIO.HIGH)
 
 def robo_right():
-    GPIO.output(motorL_pin0, GPIO.LOW)
-    GPIO.output(motorL_pin1, GPIO.HIGH)
-    GPIO.output(motorR_pin0, GPIO.HIGH)
-    GPIO.output(motorR_pin1, GPIO.LOW)
+    GPIO.output(MotorL_pin0, GPIO.LOW)
+    GPIO.output(MotorL_pin1, GPIO.HIGH)
+    GPIO.output(MotorR_pin0, GPIO.HIGH)
+    GPIO.output(MotorR_pin1, GPIO.LOW)
 
 # robo test-run
 robo_forward()
@@ -62,3 +66,9 @@ robo_stop()
 robo_right()
 time.sleep(1)
 robo_stop()
+
+# read robo-sensors
+bumper_sensor  = GPIO.input(Sensor_Front_pin) 
+falling_sensor = GPIO.input(Sensor_Sides_pin) 
+print(bumper_sensor, falling_sensor)
+
