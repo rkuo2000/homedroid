989f88e6553ce6e60c56b9ee2032db8f889277c3
Richard Kuo <rkuo2000@gmail.com>
2019-01-16 00:35:25 +0800

    Add files via upload

---
+++ gTranslator_fbchat.py
@@ -0,0 +1,83 @@
+import speech_recognition as sr
+from gtts import gTTS
+import requests
+import urllib.request
+import sys
+import os
+import random
+from fbchat import Client
+from fbchat.models import *
+
+#sl = 'en' # source language
+#tl = 'fr' # target language
+sl = sys.argv[1]
+tl = sys.argv[2]
+fb_account = sys.argv[3] # 'your_email'
+fb_passwd =  sys.argv[4] # 'your_passwd'
+
+sample_rate = 48000
+chunk_size = 512
+r= sr.Recognizer()
+
+def text2speech(text,tl):
+    tts=gTTS(text, lang=tl)
+    tts.save('gTTS.mp3')
+    #os.system('madplay gTTS.mp3') # RPi3
+    #os.system('cmdmp3 gTTS.mp3')     # PC
+    os.system('afplay gTTS.mp3')   # MAC
+    
+def speech2text():
+    with sr.Microphone(sample_rate=sample_rate, chunk_size=chunk_size) as source:
+        print("Canceling ambient noise.....")
+#        r.adjust_for_ambient_noise(source)
+        print("Speak:")
+        audio = r.listen(source)
+        print("Processing.....")
+        try:
+            text = r.recognize_google(audio, language=sl)
+            print("You said  :", text)		
+            return text
+        except sr.UnknownValueError:
+            print("Could not understand audio!")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
+def translate(text,sl,tl):
+    btext = text.encode('utf-8')
+    btext = str(btext).replace(" ","%20").replace("\\x","%")
+    text = str(btext)[2:-1]
+    url="https://translate.google.com/m?hl=%s&sl=%s&q=%s" % (tl, sl, text)
+    C_agent = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.165063 Safari/537.36 AppEngine-Google."}
+    flag = 'class="t0">'
+    request = urllib.request.Request(url, headers=C_agent)
+    page = str(urllib.request.urlopen(request).read().decode(sys.getfilesystemencoding()))
+    result = page[page.find(flag) + len(flag):]
+    result = result.split("<")[0]
+    print("Translated:", result)
+    return result
+
+def fb_send(text):
+    client.send(Message(text=text),thread_id=client.uid, thread_type=ThreadType.USER)    
+
+def greet(t):
+    i = random.randint(0,len(greeting[t])-1)
+    #os.system('madplay mp3/'+greeting[t][i]) # RPi3
+    #os.system('cmdmp3win mp3/'+greeting[t][i]))    # PC
+    os.system('afplay mp3/'+greeting[t][i])   # MAC
+
+# Main Program
+client = Client(fb_account, fb_passwd)
+print("--------------------------------------")
+
+while True:
+    # Speech to Text
+    text  = speech2text()
+    fb_send(text)
+    if text is not None:
+        ttext = translate(text,sl,tl)
+        fb_send(ttext)
+        text2speech(ttext,tl)
+    if text=="have a good day":
+        break
+    print("--------------------------------------")
+client.logout()
