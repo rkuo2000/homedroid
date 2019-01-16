d7643c8ae510193afaa080748625d4b24d48e356
Richard Kuo <rkuo2000@gmail.com>
2019-01-16 17:19:12 +0800

    Update gSR.py

--- gSR.py
+++ gSR.py
@@ -13,7 +13,7 @@ with sr.Microphone(sample_rate=sample_rate, chunk_size=chunk_size) as source:
     r.adjust_for_ambient_noise(source)
     print("Speak:")
     audio = r.listen(source)
-    print("Processing...")
+    print("Recognizing...")
     try:
         text = r.recognize_google(audio, language=sl)
         print("You said:", text)
