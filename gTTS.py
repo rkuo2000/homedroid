4f5feb7c9db77453343f676b0d28242ed5cedb89
Richard Kuo <rkuo2000@gmail.com>
2019-01-16 17:10:29 +0800

    Update gTTS.py

--- gTTS.py
+++ gTTS.py
@@ -10,8 +10,7 @@ sl = sys.argv[2]
 
 tts = gTTS(text,lang=sl)
 tts.save('gTTS.mp3')
-#os.system('mpg321 gTTS.mp3')  # PiZero
-#os.system('madplay gTTS.mp3') # RPi3
+#os.system('mpg321 gTTS.mp3') # RPi
 os.system('cmdmp3 gTTS.mp3')  # PC
 #os.system('afplay gTTS.mp3')  # MAC
 
