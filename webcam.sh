bfb9f38840f4dfd45e63c582ece89df637a8843a
Richard Kuo <rkuo2000@gmail.com>
2019-01-25 22:35:52 +0800

    Update webcam.sh

--- webcam.sh
+++ webcam.sh
@@ -1,2 +1,1 @@
-mjpg_streamer -i "input_uvc.so -d /dev/video0 -yuv -r 640x480 -f 15" -o "output_http.so -p 8080 -w /www/webcam" &
-
+mjpg_streamer -i "input_uvc.so -d /dev/video0 -y -n -r 640x480 -f 15" -o "output_http.so -p 8000 -w /www/webcam" &
