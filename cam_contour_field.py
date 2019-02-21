07fbd3dcc6512c16a137a3a4ee4623befffd8ca8
Richard Kuo <rkuo2000@gmail.com>
2019-02-21 11:05:17 +0800

    Update cam_contour_field.py

--- cam_contour_field.py
+++ cam_contour_field.py
@@ -10,19 +10,19 @@ while(1):
     height,width,channels = frame.shape
     roi = frame
     #roi = frame[int(height*0.7):height, int(width*0.1):int(width*0.9)]
-    cv2.imshow('ROI', roi)
+    #cv2.imshow('ROI', roi)
 	
     gray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (11,11), 0)
     edge = cv2.Canny(blur, 20, 160)    
-    cv2.imshow('EDGE', edge)
+    #cv2.imshow('EDGE', edge)
 	
     _, cnts, hierarchy = cv2.findContours(edge,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 	
     for c in cnts:
         (x,y,w,h) = cv2.boundingRect(c)        
         area=w*h
-        if(area<10000 ):
+        if(area>20000 ):
             continue
         cv2.rectangle(roi, (x,y), (x+w, y+h), (0,255,0), 3)
         cv2.circle(roi, (int(x+w/2),int(y+h/2)), 5, (0,0,255), 5)
