121d2f1a981d236311ad87e4b6218deaa573a6b4
Richard Kuo <rkuo2000@gmail.com>
2019-09-03 03:43:06 +0800

    Add files via upload

--- gSR.py
+++ gSR.py
@@ -1,5 +1,6 @@
 ### $pip3 install SpeechRecognition
 ### $pip3 install PyAudio
+### Usage : python gSR.py en
 import speech_recognition as sr
 import sys
 
@@ -7,10 +8,11 @@ sl = sys.argv[1]
 recognizer = sr.Recognizer()
 microphone = sr.Microphone()
 
-while True:	
-    with microphone as source:
-        recognizer.adjust_for_ambient_noise(source)
-        print("Speak:")
+with microphone as source:
+    recognizer.adjust_for_ambient_noise(source)
+
+    while True:
+        print("Speak:")	
         audio = recognizer.listen(source)
         try:
             text = recognizer.recognize_google(audio, language=sl)
@@ -19,4 +21,4 @@ while True:
             print("Could not understand audio!")
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
-    print("")
+        print("")
