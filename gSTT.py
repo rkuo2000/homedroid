06366c86ff897561e2ee1e33673f91b5266a3320
Richard Kuo <rkuo2000@gmail.com>
2019-09-03 04:04:40 +0800

    Add files via upload

--- gSTT.py
+++ gSTT.py
@@ -1,3 +1,4 @@
+### Usage: python gSTT.py en
 import speech_recognition as sr
 from gtts import gTTS
 import requests
@@ -9,35 +10,35 @@ import random
 sl = sys.argv[1]
 recognizer = sr.Recognizer()
 microphone = sr.Microphone()
-count = 0
 
 def text2speech(text,tl):
-    global count
     tts=gTTS(text, lang=tl)
-    filename='gSTT'+str(count)+'.mp3'
+    filename='gSTT.mp3'
     tts.save(filename)
-    #os.system('mpg321 '+filename)  # RPi3
+    #os.system('mpg321 '+filename)  # PiZero
+    #os.system('madplay '+filename) # RPi3
     os.system('cmdmp3 '+filename)  # PC
     #os.system('afplay '+filename)  # MAC
     os.remove(filename)
-    count += 1
     
-def speech2text():
-    with microphone as source:
-        recognizer.adjust_for_ambient_noise(source)
-        print("Speak:")
-        audio = recognizer.listen(source)
-        try:
-            text = recognizer.recognize_google(audio, language=sl)
-            print("You said:", text)
-            return text
-        except sr.UnknownValueError:
-            print("Could not understand audio!")
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
+def speech2text():    
+    print("Speak:")
+    audio = recognizer.listen(source)
+    try:
+        text = recognizer.recognize_google(audio, language=sl)
+        print("You said:", text)
+        return text
+    except sr.UnknownValueError:
+        print("Could not understand audio!")
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
 
 if __name__ == "__main__":
-    while True:
-        text  = speech2text()
-        if text is not None:
-            text2speech(text,sl)
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)    
+        while True:
+            text  = speech2text()
+            if text is not None:
+                text2speech(text,sl)
+            if text=='exit':
+                break;
